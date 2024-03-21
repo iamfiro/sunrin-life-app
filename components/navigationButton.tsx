be38609907a51a2cb740e04753d653e5ef11a698
iamfiro <appturbo102@gmail.com>
2024-03-21 16:37:35 +0900

    💄 Update NavigationButton and ScreenArticle components

--- components/navigationButton.tsx
+++ components/navigationButton.tsx
@@ -9,10 +9,7 @@ interface NavigationButtonProps {
 export default function NavigationButton({ name }: NavigationButtonProps) {
     return (
         <TouchableOpacity style={style.container}>
-            <Icon name="left" size={20} color="#000" />
-            <View style={{ width: 10 }} />
-            <Title size={5} color="#000" weight="300">{name}</Title>
-            <Title size={5} color="#9f9f9f" weight="300">으로 이동하기</Title>
+            <Icon name="left" size={25} color="#797979" />
         </TouchableOpacity>
     )
 }
@@ -20,9 +17,9 @@ export default function NavigationButton({ name }: NavigationButtonProps) {
 const style = StyleSheet.create({
     container: {
         borderColor: "#ededed",
-        borderBottomWidth: 1,
+        // borderBottomWidth: 1,
 
-        height: 50,
+        height: 70,
 
         alignItems: "center",
 
--- screens/article.tsx
+++ screens/article.tsx
@@ -36,21 +36,19 @@ export default function ScreenArticle({ route, navigation }: any) {
         <>
         <ScrollView style={style.container} onLayout={onLayoutRootView}>
             <NavigationButton name="홈" />
-            <View style={{ paddingHorizontal: 17.5, marginTop: 20 }}>
-                <View style={{ flexDirection: 'row', justifyContent: "space-between" }}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Title size={6} color="#7a7a7a" weight="200">10413 이태겸</Title>
-                        <View style={{ width: 8 }} />
-                        <Badge type="president" />
-                    </View>
-                    <Title size={5} color="#000000" weight="200">📄  숙제</Title>
-                </View>
-                <View style={{ height: 15 }} />
+            <View style={{ paddingHorizontal: 17.5, marginTop: 10 }}>
+                <Title size={6} color="#000000" weight="200">📄  숙제</Title>
+                <View style={{ height: 10 }} />
                 <Title size={3} color="#000" weight="300">영어 3245p 숙제</Title>
                 <Text style={{ fontSize: 16, color: '#585858', fontFamily: 'Wanted Sans Regular', lineHeight: 23, marginTop: 15 }}>국어 전공관련책 2권읽기(8.15){'\n\n'}영어 멘토멘티 최대한 해오기{'\n\n'}컴시일 캔바를이용하여 교과서 한단원 정리후 제출하기(3.18){'\n'}디일 스프링무선노트 가져오기(3.18){'\n\n'}반장 찬양하고 잠들기{'\n\n'}가정통신문{'\n'}-행정정보 공동이용서{'\n'}-방과후 신청서{'\n\n'}급식{'\n'}-후리카케작은밥(?)어묵국{'\n'}국물떡볶이{'\n'}찐순대{'\n'}분식튀김(만두,어묵,김말이){'\n'}단무지{'\n'}복숭아에이드(오우예ㅔㅔ){'\n'}대체찬(백미밥)</Text>
-                <View style={{ flexDirection: 'row', marginTop: 15 }}>
-                    <Title size={6} color="#838383" weight="200">2024-03-15  ·  23명 조회함  ·  </Title>
-                    <Title size={6} color="#0080ff" weight="200">#📄 숙제</Title>
+                <View style={{ marginTop: 15 }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Title size={6} color="#7a7a7a" weight="200">10413 이태겸</Title>
+                        <View style={{ width: 5 }} />
+                        <Badge type="president" />
+                    </View>
+                    <View style={{ height: 7 }} />
+                    <Title size={6} color="#838383" weight="200">2024-03-15  ·  23명 조회함</Title>
                 </View>
             </View>
         </ScrollView>
