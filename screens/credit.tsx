62b7138e31024151c07b4d202b0616cc481ebccd
iamfiro <appturbo102@gmail.com>
2024-03-23 20:07:55 +0900

    💄 Add credit.tsx

---
+++ screens/credit.tsx
@@ -0,0 +1,14 @@
+import { Text } from "react-native";
+
+/**
+ * Renders the widget setting screen.
+ * 
+ * @returns The JSX element representing the widget setting screen.
+ */
+export default function ScreenWidgetSetting() {
+    return (
+        <>
+            <Text>Setting</Text>
+        </>
+    )
+}
