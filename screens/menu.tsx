f2aeeefc4f7b0814e37cda6b430dae143fc7f94a
iamfiro <appturbo102@gmail.com>
2024-03-23 19:31:00 +0900

    💄 Update dependencies and add Instagram image to menu screen

--- screens/menu.tsx
+++ screens/menu.tsx
@@ -8,6 +8,7 @@ import Title from "../components/title";
 import NavigationButton from "../components/navigationButton";
 import Icon from "react-native-vector-icons/AntDesign";
 import WidgetImage from '../assets/icon/menu/widget.png';
+import InstagramImage from '../assets/icon/menu/instagram.png';
 import Button from "../components/button";
 import { FontList } from ".";
 
@@ -69,14 +70,17 @@ export default function ScreenMenu({ navigation }: IDefaultScreenProps) {
             <View style={{ paddingHorizontal: 17.5, marginTop: 10 }}>
                 <Title size={3} color="#000000" weight="400">기본 설정</Title>
                 <View style={{ height: 20 }} />
-                <MenuItem title="메인 화면 위젯 설정" icon={WidgetImage} onPress={() => navigation.navigate("ArticleList")} />
-                <MenuItem title="메인 화면 위젯 설정" icon={WidgetImage} onPress={() => navigation.navigate("ArticleList")} />
+                <MenuItem title="메인 화면 위젯 설정" icon={WidgetImage} onPress={() => navigation.navigate("WidgetSetting")} />
+                <View style={{ height: 20 }} />
+                <Title size={3} color="#000000" weight="400">기타</Title>
+                <View style={{ height: 20 }} />
+                <MenuItem title="공식 인스타그램" icon={InstagramImage} onPress={() => Linking.openURL("https://www.instagram.com/sunrin_life")} />
                 <View style={{ height: 20 }} />
                 <Button type="primary" text="문의하기" onClick={() => { Linking.openURL("https://www.instagram.com/sunrin_life")}} />
                 <View style={{ height: 5 }} />
-                <Button type="secondary" text="앱 로그아웃" onClick={() => { Linking.openURL("https://www.instagram.com/sunrin_life")}} />
-                <View style={{ height: 20 }} />
-                <Title size={3} color="#000000" weight="400"></Title>
+                <Button type="secondary" text="앱 로그아웃" onClick={() => { }} />
+                <View style={{ height: 30 }} />
+                <Title size={6} color="#9d9d9d" weight="200">애플리케이션 버전 1.2.18</Title>
             </View>
         </ScrollView>
         <BottomNavigation pageName="Menu" navigation={navigation} />
