28122c67c446d636be95b07325e059c764fee5a7
iamfiro <appturbo102@gmail.com>
2024-03-24 01:13:20 +0900

    🔥 Remove Sentry integration and update app.json

--- App.tsx
+++ App.tsx
@@ -8,25 +8,9 @@ import ScreenMenu from "./screens/menu";
 import ScreenWidgetSetting from "./screens/widgetSetting";
 import ScreenCommunity from "./screens/community";
 import ScreenWelcome from "./screens/welcome";
-import * as Sentry from '@sentry/react-native';
 import ScreenCredit from "./screens/credit";
-import { useCallback } from "react";
-import { FontList } from "./screens";
 import * as SplashScreen from "expo-splash-screen";
-import { useFonts } from "expo-font";
 import ScreenLoading from "./screens/loading";
-
-const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  debug: false,
-  integrations: [
-    new Sentry.ReactNativeTracing({
-      routingInstrumentation,
-    }),
-  ],
-});
 
 const Stack = createNativeStackNavigator();
 
@@ -51,4 +35,4 @@ function App() {
     );
 }
 
-export default Sentry.wrap(App)
+export default App;
