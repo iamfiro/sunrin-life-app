00d2a36977c8890a82e1dcac6d426519d085dcc6
iamfiro <appturbo102@gmail.com>
2024-03-24 16:26:17 +0900

    💄 Add menu items and icons

--- screens/menu.tsx
+++ screens/menu.tsx
@@ -7,7 +7,13 @@ import Icon from "react-native-vector-icons/AntDesign";
 import WidgetImage from '../assets/icon/menu/widget.png';
 import InstagramImage from '../assets/icon/menu/instagram.png';
 import DeveloperImage from '../assets/icon/menu/developer.png';
+import GithubImage from '../assets/icon/menu/github.png'
 import Button from "../components/button";
+import FoodImage from "../assets/icon/menu/food.png";
+import WeatherImage from "../assets/icon/menu/weather.png";
+import TrophyImage from "../assets/icon/trophy.png";
+import CalenderImage from "../assets/icon/calender.png";
+import BearImage from "../assets/icon/menu/bear.png";
 
 /* Represents the props for a menu item. */
 interface MenuItemProps {
@@ -54,16 +60,25 @@ export default function ScreenMenu({ navigation }: IDefaultScreenProps) {
         <ScrollView style={style.container}>
             <NavigationButton onClick={() => navigation.navigate("Home")} />
             <View style={{ paddingHorizontal: 17.5, marginTop: 10 }}>
-                <Title size={3} color="#000000" weight="400" marginBottom={20}>기본 설정</Title>
+                <Title size={3} color="#000000" weight="400" marginBottom={20}>설정</Title>
                 <MenuItem title="메인 화면 위젯 설정" icon={WidgetImage} onPress={() => navigation.navigate("WidgetSetting")} />
+                <Title size={3} color="#000000" weight="400" marginTop={20} marginBottom={20}>학교</Title>
+                <MenuItem title="현재 선린인고의 날씨" icon={WeatherImage} onPress={() => navigation.navigate("WidgetSetting")} />
+                <MenuItem title="오늘 나오는 급식 보기" icon={FoodImage} onPress={() => navigation.navigate("WidgetSetting")} />
+                <MenuItem title="대회 일정 한 눈에 보기" icon={TrophyImage} onPress={() => navigation.navigate("WidgetSetting")} />
+                <MenuItem title="학사 일정 쉽게 보기" icon={CalenderImage} onPress={() => navigation.navigate("WidgetSetting")} />
                 <Title size={3} color="#000000" weight="400" marginBottom={20} marginTop={20}>기타</Title>
                 <MenuItem title="공식 인스타그램" icon={InstagramImage} onPress={() => Linking.openURL("https://www.instagram.com/sunrin_life")} />
+                <MenuItem title="선린라이프 Github" icon={GithubImage} onPress={() => Linking.openURL("https://github.com/sunrin-life/app")} />
                 <MenuItem title="개발자 정보" icon={DeveloperImage} onPress={() => navigation.navigate("Credit")} />
+                <Title size={3} color="#000000" weight="400" marginBottom={20} marginTop={20}>ψ(｀∇´)ψ</Title>
+                <MenuItem title="새로운 기능 제안하기" icon={BearImage} onPress={() => Linking.openURL("https://www.instagram.com/sunrin_life")} />
                 <View style={{ height: 20 }} />
                 <Button type="primary" text="문의하기" onClick={() => { Linking.openURL("https://www.instagram.com/sunrin_life")}} />
                 <View style={{ height: 5 }} />
                 <Button type="secondary" text="앱 로그아웃" onClick={() => { }} />
                 <Title size={6} color="#9d9d9d" weight="200" marginTop={20}>애플리케이션 버전 1.0.0</Title>
+                <View style={{ height: 100 }} />
             </View>
         </ScrollView>
         <BottomNavigation pageName="Menu" navigation={navigation} />
