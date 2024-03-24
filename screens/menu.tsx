3bd16ac2e717eab9e83d6b7ebae02424e2919f3e
iamfiro <appturbo102@gmail.com>
2024-03-24 15:46:02 +0900

    💄 Update application version title margin

--- screens/menu.tsx
+++ screens/menu.tsx
@@ -63,7 +63,7 @@ export default function ScreenMenu({ navigation }: IDefaultScreenProps) {
                 <Button type="primary" text="문의하기" onClick={() => { Linking.openURL("https://www.instagram.com/sunrin_life")}} />
                 <View style={{ height: 5 }} />
                 <Button type="secondary" text="앱 로그아웃" onClick={() => { }} />
-                <Title size={6} color="#9d9d9d" weight="200" marginTop={30}>애플리케이션 버전 1.0.0</Title>
+                <Title size={6} color="#9d9d9d" weight="200" marginTop={20}>애플리케이션 버전 1.0.0</Title>
             </View>
         </ScrollView>
         <BottomNavigation pageName="Menu" navigation={navigation} />
