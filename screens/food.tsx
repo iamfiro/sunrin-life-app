0152ab3b1b5b58d0100cc823e6288cfce97a1786
iamfiro <appturbo102@gmail.com>
2024-03-24 16:38:15 +0900

    ✨ Add base `food.tsx`

---
+++ screens/food.tsx
@@ -0,0 +1,26 @@
+import { ScrollView, StatusBar, StyleSheet } from "react-native";
+import { IDefaultScreenProps } from "../types/screen";
+
+/**
+ * Renders the widget setting screen.
+ * 
+ * @returns The JSX element representing the widget setting screen.
+ */
+export default function ScreenFood({ navigation }: IDefaultScreenProps) {
+    return (
+        <>
+            <ScrollView style={style.container}>
+                
+            </ScrollView>
+            <StatusBar backgroundColor={"#ffffff"} barStyle={"dark-content"} />
+        </>
+    )
+}
+
+const style = StyleSheet.create({
+    /* The container style for the menu screen. */
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+});
