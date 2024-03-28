a25707891c863bdcc29cbbf7a81a59458ec321f5
iamfiro <appturbo102@gmail.com>
2024-03-28 20:35:07 +0900

    💄 Add text prop to NavigationButton component

--- components/navigationButton.tsx
+++ components/navigationButton.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
+import Title from "./title";
 
 /**
  * Props for the NavigationButton component.
  */
 interface NavigationButtonProps {
     onClick: () => void;
+    text?: string;
 }
 
 /**
@@ -14,10 +16,12 @@ interface NavigationButtonProps {
  * @param onClick - The function to be called when the button is clicked.
  * @returns The rendered navigation button component.
  */
-export default function NavigationButton({ onClick }: NavigationButtonProps) {
+export default function NavigationButton({ onClick, text }: NavigationButtonProps) {
     return (
         <TouchableOpacity style={style.container} onPress={() => onClick()}>
             <Icon name="left" size={25} color="#797979" />
+            <View style={{ width: 15 }} />
+            <Title size={4} color="#000000" weight="300">{text}</Title>
         </TouchableOpacity>
     )
 }
